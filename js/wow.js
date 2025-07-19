7b98ad701fe43e961edf56b41286a38a0c319d62
Arthur Hozanna <90093341+arthurhzna@users.noreply.github.com>
2025-07-19 14:47:02 +0700

    Update wow.js

--- js/wow.js
+++ js/wow.js
@@ -1,4 +1,4 @@
-(function() {
+(function() { 
   var MutationObserver, Util, WeakMap, getComputedStyle, getComputedStyleRX,
     on = function(fn, me){ return function(){ return fn.apply(me, arguments); }; },
     indexOf = [].indexOf || function(item) { for (var i = 0, l = this.length; i < l; i++) { if (i in this && this[i] === item) return i; } return -1; };
